8fac78b75946d47ce8034e5bd8ad8db0baf12751
simonnielsen1307-spec <simonnielsen1307@gmail.com>
2026-06-05 21:07:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'dystrup-v1';
+const ASSETS = [
+  '/dystrupjagt/',
+  '/dystrupjagt/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
